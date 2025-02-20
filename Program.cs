a73f205a0c4f8f80279f58d075e5868f39ebc3fe
Muhammadjon <muhammadyashnar2003@gmail.com>
2025-02-20 12:41:52 +0500

    1-loyiha.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
 ﻿using System;
 
+Console.WriteLine("< Bu \"DOIRA PARAMETRLARI\" dasturi >");
+
+Console.Write("Doira radiusini kiriting:\n>>> ");
+double radius = Convert.ToDouble(Console.ReadLine());
+
+// O'zgaruvchilar
+const double PI = 3.14159265359;
+double yuza = PI * radius * radius;
+double uzunlik = 2 * PI * radius;
+
+Console.WriteLine($"Doira yuzi: {yuza}.");
+Console.WriteLine($"Aylanasi uzunligi: {uzunlik}.");
